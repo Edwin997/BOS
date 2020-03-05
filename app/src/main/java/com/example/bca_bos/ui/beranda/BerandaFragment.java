a1d4b321c021e4886c885ae3f9deaff624628ec2
Mahendra Tirta Saputra Trimarmanto <mahendra.tirta@yahoo.com>
2020-03-05 10:48:53 +0700

    Menambah popup detail pembeli pada beranda

--- app/src/main/java/com/example/bca_bos/ui/beranda/BerandaFragment.java
+++ app/src/main/java/com/example/bca_bos/ui/beranda/BerandaFragment.java
@@ -21,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.bca_bos.Method;
 import com.example.bca_bos.R;
 import com.example.bca_bos.interfaces.OnCallBackListener;
+import com.example.bca_bos.models.Pembeli;
 import com.example.bca_bos.models.Produk;
 import com.makeramen.roundedimageview.RoundedImageView;
 
@@ -35,38 +36,54 @@ public class BerandaFragment extends Fragment implements View.OnClickListener, O
     private BerandaPembeliAdapter g_beranda_pembeli_adapter;
     private Button g_beranda_pesanan_belum_dikirim_button;
 
-    //Produk Dialog Box
+    //Produk Popup
     private Dialog g_beranda_produk_popup;
-    private RoundedImageView g_beranda_popup_gambar;
-    private TextView g_beranda_popup_tv_nama, g_beranda_popup_tv_harga, g_beranda_popup_tv_stok;
-    private Button g_beranda_popup_btn_close;
+    private RoundedImageView g_beranda_produk_popup_gambar;
+    private TextView g_beranda_produk_popup_tv_nama, g_beranda_produk_popup_tv_harga, g_beranda_produk_popup_tv_stok;
+    private Button g_beranda_produk_popup_btn_close;
+
+    //Pembeli Popup
+    private Dialog g_beranda_pembeli_popup;
+    private TextView g_beranda_pembeli_popup_tv_nama, g_beranda_pembeli_popup_tv_nominal, g_beranda_pembeli_popup_tv_transaksi;
+    private Button g_beranda_pembeli_popup_btn_close;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         g_context = container.getContext();
         g_view = inflater.inflate(R.layout.fragment_beranda, container, false);
 
-        //Set Produk RecyclerView
+        //TRANSAKSI
+        g_beranda_pesanan_belum_dikirim_button = g_view.findViewById(R.id.beranda_btn_pesanan_belum_dikirim);
+        g_beranda_pesanan_belum_dikirim_button.setOnClickListener(this);
+
+        //PRODUK
+        //RecyclerView
         g_beranda_produk_fragment_recyclerview = g_view.findViewById(R.id.apps_beranda_produk_fragment_recyclerview);
         g_linearlayoutmanager_produk = new LinearLayoutManager(g_context, RecyclerView.HORIZONTAL, false);
         g_beranda_produk_adapter = new BerandaProdukAdapter();
-        g_beranda_produk_adapter.setParentOnCallBack(this);
 
         g_beranda_produk_fragment_recyclerview.setAdapter(g_beranda_produk_adapter);
         g_beranda_produk_fragment_recyclerview.setLayoutManager(g_linearlayoutmanager_produk);
 
+        //Popup
+        g_beranda_produk_adapter.setParentOnCallBack(this);
         g_beranda_produk_popup = new Dialog(g_context);
 
-        //Set Pembeli RecyclerView
+        //PEMBELI
+        //RecyclerView
         g_beranda_pembeli_fragment_recyclerview = g_view.findViewById(R.id.apps_beranda_pembeli_fragment_recyclerview);
         g_linearlayoutmanager_pembeli = new LinearLayoutManager(g_context);
         g_beranda_pembeli_adapter = new BerandaPembeliAdapter();
 
+
         g_beranda_pembeli_fragment_recyclerview.setAdapter(g_beranda_pembeli_adapter);
         g_beranda_pembeli_fragment_recyclerview.setLayoutManager(g_linearlayoutmanager_pembeli);
 
-        g_beranda_pesanan_belum_dikirim_button = g_view.findViewById(R.id.beranda_btn_pesanan_belum_dikirim);
-        g_beranda_pesanan_belum_dikirim_button.setOnClickListener(this);
+        //Popup
+        g_beranda_pembeli_adapter.setParentOnCallBack(this);
+        g_beranda_pembeli_popup = new Dialog(g_context);
+
+
 
         return g_view;
     }
@@ -83,6 +100,9 @@ public class BerandaFragment extends Fragment implements View.OnClickListener, O
             case R.id.btn_popup_beranda_produk_close:
                 g_beranda_produk_popup.dismiss();
                 break;
+            case R.id.btn_popup_beranda_pembeli_close:
+                g_beranda_pembeli_popup.dismiss();
+                break;
         }
     }
 
@@ -91,26 +111,45 @@ public class BerandaFragment extends Fragment implements View.OnClickListener, O
         if(p_obj instanceof Produk){
             Produk l_produk = (Produk) p_obj;
             showProdukPopUp(l_produk);
+        }else if (p_obj instanceof Pembeli){
+            Pembeli l_pembeli = (Pembeli) p_obj;
+            showPembeliPopUp(l_pembeli);
         }
     }
 
     public void showProdukPopUp(Produk p_produk){
-        g_beranda_produk_popup.setContentView(R.layout.layout_popup_bernada_produk);
+        g_beranda_produk_popup.setContentView(R.layout.layout_popup_beranda_produk);
 
-        g_beranda_popup_gambar = g_beranda_produk_popup.findViewById(R.id.iv_popup_beranda_produk_item);
-        g_beranda_popup_gambar.setImageDrawable(getResources().getDrawable(p_produk.getGambar()));
+        g_beranda_produk_popup_gambar = g_beranda_produk_popup.findViewById(R.id.iv_popup_beranda_produk_item);
+        g_beranda_produk_popup_gambar.setImageDrawable(getResources().getDrawable(p_produk.getGambar()));
 
-        g_beranda_popup_tv_nama = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_nama_item);
-        g_beranda_popup_tv_nama.setText(String.valueOf(p_produk.getNama()));
-        g_beranda_popup_tv_harga = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_harga_item);
-        g_beranda_popup_tv_harga.setText(Method.getIndoCurrency(p_produk.getHarga()));
-        g_beranda_popup_tv_stok = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_stok_item);
-        g_beranda_popup_tv_stok.setText("Stok : "+String.valueOf(p_produk.getStok()));
-        g_beranda_popup_btn_close = g_beranda_produk_popup.findViewById(R.id.btn_popup_beranda_produk_close);
-        g_beranda_popup_btn_close.setOnClickListener(this);
+        g_beranda_produk_popup_tv_nama = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_nama_item);
+        g_beranda_produk_popup_tv_nama.setText(String.valueOf(p_produk.getNama()));
+        g_beranda_produk_popup_tv_harga = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_harga_item);
+        g_beranda_produk_popup_tv_harga.setText(Method.getIndoCurrency(p_produk.getHarga()));
+        g_beranda_produk_popup_tv_stok = g_beranda_produk_popup.findViewById(R.id.tv_popup_beranda_produk_stok_item);
+        g_beranda_produk_popup_tv_stok.setText("Stok : "+String.valueOf(p_produk.getStok()));
+        g_beranda_produk_popup_btn_close = g_beranda_produk_popup.findViewById(R.id.btn_popup_beranda_produk_close);
+        g_beranda_produk_popup_btn_close.setOnClickListener(this);
 
         g_beranda_produk_popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         g_beranda_produk_popup.show();
 
+    }
+
+    public void showPembeliPopUp(Pembeli p_pembeli){
+        g_beranda_pembeli_popup.setContentView(R.layout.layout_popup_beranda_pembeli);
+
+        g_beranda_pembeli_popup_tv_nama = g_beranda_pembeli_popup.findViewById(R.id.tv_popup_beranda_pembeli_nama_item);
+        g_beranda_pembeli_popup_tv_nama.setText(String.valueOf(p_pembeli.getNama()));
+        g_beranda_pembeli_popup_tv_nominal = g_beranda_pembeli_popup.findViewById(R.id.tv_popup_beranda_pembeli_nominal_item);
+        g_beranda_pembeli_popup_tv_nominal.setText(Method.getIndoCurrency(p_pembeli.getNominalTransaksi()));
+        g_beranda_pembeli_popup_tv_transaksi = g_beranda_pembeli_popup.findViewById(R.id.tv_popup_beranda_pembeli_transaksi_item);
+        g_beranda_pembeli_popup_tv_transaksi.setText(String.valueOf(p_pembeli.getJumlahTransaksi()+" transaksi"));
+        g_beranda_pembeli_popup_btn_close = g_beranda_pembeli_popup.findViewById(R.id.btn_popup_beranda_pembeli_close);
+        g_beranda_pembeli_popup_btn_close.setOnClickListener(this);
+
+        g_beranda_pembeli_popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        g_beranda_pembeli_popup.show();
     }
 }
--- app/src/main/java/com/example/bca_bos/ui/beranda/BerandaPembeliAdapter.java
+++ app/src/main/java/com/example/bca_bos/ui/beranda/BerandaPembeliAdapter.java
@@ -3,6 +3,7 @@ package com.example.bca_bos.ui.beranda;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.bca_bos.Method;
 import com.example.bca_bos.dummy.ListPembeliDummy;
 import com.example.bca_bos.R;
+import com.example.bca_bos.interfaces.OnCallBackListener;
 import com.example.bca_bos.models.Pembeli;
 
 import java.util.List;
 
-public class BerandaPembeliAdapter extends RecyclerView.Adapter<BerandaPembeliAdapter.BerandaPembeliViewHolder> {
+public class BerandaPembeliAdapter extends RecyclerView.Adapter<BerandaPembeliAdapter.BerandaPembeliViewHolder> implements OnCallBackListener {
 
     private List<Pembeli> g_list_pembeli;
+    private OnCallBackListener g_parent_oncallbacklistener;
 
     public BerandaPembeliAdapter(){
         g_list_pembeli = ListPembeliDummy.pembeliList;
@@ -30,7 +33,7 @@ public class BerandaPembeliAdapter extends RecyclerView.Adapter<BerandaPembeliAd
         View l_view = tmp_layout.inflate(R.layout.item_apps_beranda_pembeli, parent, false);
 
         BerandaPembeliAdapter.BerandaPembeliViewHolder tmpHolder = new BerandaPembeliAdapter.BerandaPembeliViewHolder(l_view);
-//        tmpHolder.setParentOnCallBack(this);
+        tmpHolder.setParentOnCallBack(this);
         return tmpHolder;
     }
 
@@ -44,23 +47,57 @@ public class BerandaPembeliAdapter extends RecyclerView.Adapter<BerandaPembeliAd
         return g_list_pembeli.size();
     }
 
-    public class BerandaPembeliViewHolder extends RecyclerView.ViewHolder {
+    public void setParentOnCallBack(OnCallBackListener p_oncallback){
+        this.g_parent_oncallbacklistener = p_oncallback;
+    }
+
+    @Override
+    public void OnCallBack(Object p_obj) {
+        if(g_parent_oncallbacklistener != null){
+            g_parent_oncallbacklistener.OnCallBack(p_obj);
+        }
+    }
+
+    public class BerandaPembeliViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private LinearLayout l_ll_container_beranda_pembeli;
 
         private TextView l_tv_beranda_pembeli_nama, l_tv_beranda_pembeli_transaksi, l_tv_beranda_pembeli_nominal;
+        private Pembeli l_pembeli;
+
+        private OnCallBackListener l_parent_oncallbacklistener;
 
         public BerandaPembeliViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            l_ll_container_beranda_pembeli = itemView.findViewById(R.id.ll_apps_beranda_pembeli_item);
             l_tv_beranda_pembeli_nama = itemView.findViewById(R.id.tv_apps_beranda_pembeli_nama_item);
             l_tv_beranda_pembeli_transaksi = itemView.findViewById(R.id.tv_apps_beranda_pembeli_transaksi_item);
             l_tv_beranda_pembeli_nominal = itemView.findViewById(R.id.tv_apps_beranda_pembeli_nominal_item);
 
+            l_ll_container_beranda_pembeli.setOnClickListener(this);
+
         }
 
         public void setData(Pembeli pembeli){
+            l_pembeli = pembeli;
+
             l_tv_beranda_pembeli_nama.setText(pembeli.getNama());
             l_tv_beranda_pembeli_transaksi.setText(String.valueOf(pembeli.getJumlahTransaksi()));
             l_tv_beranda_pembeli_nominal.setText(Method.getIndoCurrency(pembeli.getNominalTransaksi()));
+        }
+
+        public void setParentOnCallBack(OnCallBackListener p_oncallback){
+            this.l_parent_oncallbacklistener = p_oncallback;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(view == l_ll_container_beranda_pembeli){
+                if(l_parent_oncallbacklistener != null){
+                    l_parent_oncallbacklistener.OnCallBack(l_pembeli);
+                }
+            }
         }
     }
 }
