6c7aa936f0be729b2f3db414e3e0a3cc5dbec609
Edwin <edwinhardiatno09@gmail.com>
2020-03-11 16:11:20 +0700

    Selesai connect ke be (templated text dan profile) 2

--- app/src/main/java/com/example/bca_bos/models/locations/Address.java
+++ app/src/main/java/com/example/bca_bos/models/locations/Address.java
@@ -1,4 +1,27 @@
 package com.example.bca_bos.models.locations;
 
+import com.example.bca_bos.models.Buyer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Address {
+    private int id_address;
+    private String address; //100
+    private Buyer buyer;
+    private Kelurahan kelurahan;
+
+    public Address(String p_address, Buyer p_buyer, Kelurahan p_kelurahan){
+        this.setAddress(p_address);
+        this.setBuyer(p_buyer);
+        this.setKelurahan(p_kelurahan);
+    }
+
 }
